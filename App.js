bb968d83f79aef93c3f41ade3528f9bb22f8b765
Xavier Perez <xap5xap@users.noreply.github.com>
2017-10-24 14:38:02 -0500

    Add sign in

--- App.js
+++ App.js
@@ -61,11 +61,54 @@ export default class App extends Component<{}> {
 
   }
 
+  confirmCode() {
+    const cognitoUser = new CognitoUser({
+      Username: this.username,
+      Pool: this.userPool
+    });
+    cognitoUser.confirmRegistration('091758', true, (err, result) => {
+      if (err) {
+        console.log('Error at confirmRegistration ', err);
+        return;
+      }
+      console.log('result', result)
+    });
+  }
+
+  signIn() {
+    const authenticationDetails = new AuthenticationDetails({
+      Username: this.username,
+      Password: this.password
+    });
+    const cognitoUser = new CognitoUser({
+      Username: this.username,
+      Pool: this.userPool
+    });
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: (result) => {
+        console.log('onSuccess', result)
+        console.log('access token + ' + result.getAccessToken().getJwtToken());
+      },
+
+      onFailure: (err) => {
+        console.log('onFailure', err)
+      },
+      mfaRequired: (codeDeliveryDetails) => {
+        console.log('mfaRequired', codeDeliveryDetails)
+      }
+    });
+
+  }
+
 
   render() {
     return (
       <View style={styles.container}>
         <Button title="1) Create user in Amazon Cognito (Sign up)" onPress={this.createUserInAmazonCognito.bind(this)}>
+        </Button>
+        <Button title="2) Confirm Code" onPress={this.confirmCode.bind(this)}>
+        </Button>
+        <Button title="3) Sign in" onPress={this.signIn.bind(this)}>
         </Button>
       </View>
     );
